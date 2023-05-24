22c9574b88f504cc28baea285894cc09cb756b25
Christopher Pierce <verdurous.lazuli@gmail.com>
2023-05-24 01:19:09 -0700

    Added edit functionality to profile Changed gallery item implementation into templates via JS.

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,71 @@
 const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    link: "./images/yosemite-valley.png",
   },
   {
     name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    link: "./images/lake-louise.png",
   },
   {
     name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    link: "./images/bald-mountains.png",
   },
   {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    link: "./images/latemar.png",
   },
   {
     name: "Vanoise National Park",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    link: "./images/vanoise-national-park.png",
   },
   {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    link: "./images/lago-di-braies.png",
   },
 ];
+let itemTemplate = document.querySelector("#item__template").content;
+let galleryList = document.querySelector(".gallery");
+function getGalleryElement(nm, lk) {
+  let itemTemplate = document.querySelector("#item__template").content;
+  let galleryItem = itemTemplate
+    .querySelector(".gallery__item")
+    .cloneNode(true);
+  let galleryImage = galleryItem.querySelector(".gallery__image");
+  let galleryTitle = galleryItem.querySelector(".gallery__title");
+  galleryImage.src = lk;
+  galleryImage.alt = nm;
+  galleryTitle.textContent = nm;
+  return galleryItem;
+}
+for (let i = 0; i < initialCards.length; i++) {
+  let galleryItem = getGalleryElement(
+    initialCards[i].name,
+    initialCards[i].link
+  );
+  galleryList.append(galleryItem);
+}
+
 let modal = document.querySelector(".modal");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".modal__close-button");
+const formElement = document.querySelector(".modal__form");
+const nameInput = document.querySelector("#name-input");
+const jobInput = document.querySelector("#job-input");
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__description");
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
 editButton.addEventListener("click", () => {
   modal.classList.add("modal_opened");
 });
 closeButton.addEventListener("click", () => {
   modal.classList.remove("modal_opened");
 });
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  modal.classList.remove("modal_opened");
+}
+formElement.addEventListener("submit", formSubmitHandler);
