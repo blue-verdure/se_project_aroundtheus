a746c5626586b5ba5a2002823af68f8580dea849
Christopher Pierce <verdurous.lazuli@gmail.com>
2023-09-23 09:47:36 -0700

    condensed click event listeners & esc function

--- scripts/index.js
+++ scripts/index.js
@@ -25,23 +25,20 @@ const initialCards = [
   },
 ];
 const itemTemplate = document.querySelector("#item__template").content;
-const galleryList = document.querySelector(".gallery");
+const gallery = document.querySelector(".gallery");
 const modalEdit = document.querySelector(".modal-edit");
 const modalAdd = document.querySelector(".modal-add");
 const editButton = document.querySelector(".profile__edit-button");
-const closeButtonEdit = modalEdit.querySelector(".modal__close-button");
-const closeButtonAdd = modalAdd.querySelector(".modal__close-button");
 const addButton = document.querySelector(".profile__add-button");
 const formElementEdit = document.querySelector(".modal__form");
 const formElementAdd = document.querySelector(".modal__form_add");
 const nameInput = document.querySelector("#name-input");
-const jobInput = document.querySelector("#job-input");
+const descInput = document.querySelector("#desc-input");
 const placeInput = document.querySelector("#place-input");
 const linkInput = document.querySelector("#link-input");
 const profileName = document.querySelector(".profile__title");
-const profileJob = document.querySelector(".profile__description");
+const profileDesc = document.querySelector(".profile__description");
 const imgView = document.querySelector(".modal-view");
-const imgClose = imgView.querySelector(".modal__close-button");
 
 function getGalleryElement(name, link) {
   const itemTemplate = document.querySelector("#item__template").content;
@@ -53,44 +50,27 @@ function getGalleryElement(name, link) {
   galleryImage.src = link;
   galleryImage.alt = name;
   galleryTitle.textContent = name;
-  const galleryDelete = galleryItem.querySelector(".gallery__delete-button");
-  galleryDelete.addEventListener("click", deleteCard);
-  const galleryLike = galleryItem.querySelector(".gallery__icon");
-  galleryLike.addEventListener("click", () => {
-    galleryLike.classList.toggle("gallery__icon_active");
-  });
-  galleryImage.addEventListener("click", (evt) => {
-    popupImage(evt, galleryTitle.textContent);
-  });
   return galleryItem;
 }
-
 function openModal(modal) {
   modal.classList.add("modal_opened");
 }
 function closeModal(modal) {
   modal.classList.remove("modal_opened");
 }
-
 function handleAddSubmit(evt) {
   evt.preventDefault();
-  galleryList.prepend(getGalleryElement(placeInput.value, linkInput.value));
+  gallery.prepend(getGalleryElement(placeInput.value, linkInput.value));
   closeModal(modalAdd);
   placeInput.value = "";
   linkInput.value = "";
 }
-
-function deleteCard(evt) {
-  evt.target.closest(".gallery__item").remove();
-}
-
 function handleEditSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
+  profileDesc.textContent = descInput.value;
   closeModal(modalEdit);
 }
-
 function popupImage(evt, name) {
   imgView.querySelector(".modal__image").src = evt.target.src;
   imgView.querySelector(".modal__image").alt = evt.target.alt;
@@ -98,30 +78,66 @@ function popupImage(evt, name) {
   imgTitle.textContent = name;
   openModal(imgView);
 }
+function handleOverlayClose(evt) {
+  if (evt.target.classList.contains("modal_opened")) {
+    closeModal(evt.target);
+  } else if (evt.target.classList.contains("modal__close-img")) {
+    closeModal(evt.target.closest(".modal"));
+  }
+}
+function handleEscClose(evt) {
+  if (evt.key === "Escape") {
+    const openedModal = document.querySelector(".modal_opened");
+    closeModal(openedModal);
+  }
+}
+function handleMiscClick(evt) {
+  //handler for image popup, like button, and delete button
+  if (evt.target.classList.contains("gallery__image")) {
+    popupImage(
+      evt,
+      evt.target.closest(".gallery__item").querySelector(".gallery__title")
+        .textContent
+    );
+  } else if (evt.target.classList.contains("gallery__icon")) {
+    evt.target.classList.toggle("gallery__icon_active");
+  } else if (evt.target.classList.contains("gallery__delete-icon")) {
+    evt.target.closest(".gallery__item").remove();
+  }
+}
+function setInputListeners(form) {
+  const inputList = Array.from(form.querySelectorAll(".modal__input"));
+  inputList.forEach((input) => {
+    input.addEventListener("input", () => {
+      checkValidity(form, input);
+    });
+  });
+}
+function validationHandler() {
+  const formList = Array.from(document.querySelectorAll(".modal__form"));
+  formList.forEach((form) => {
+    form.addEventListener("submit", function(evt) => {
+      evt.preventDefault();
+    });
+    setInputListeners(form);
+  }); 
+}
 
 initialCards.forEach((item) =>
-  galleryList.append(getGalleryElement(item.name, item.link))
+  gallery.append(getGalleryElement(item.name, item.link))
 );
+
+document.addEventListener("keydown", handleEscClose);
+document.addEventListener("click", handleOverlayClose);
+document.addEventListener("click", handleMiscClick);
 
 editButton.addEventListener("click", () => {
   nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+  descInput.value = profileDesc.textContent;
   openModal(modalEdit);
 });
-closeButtonEdit.addEventListener("click", () => {
-  closeModal(modalEdit);
-});
-
 formElementEdit.addEventListener("submit", handleEditSubmit);
 addButton.addEventListener("click", () => {
   openModal(modalAdd);
 });
-closeButtonAdd.addEventListener("click", () => {
-  closeModal(modalAdd);
-});
-
 formElementAdd.addEventListener("submit", handleAddSubmit);
-
-imgClose.addEventListener("click", () => {
-  closeModal(imgView);
-});
