7036677c419c148f42cf58221fea638ca9fd2726
Christopher Pierce <verdurous.lazuli@gmail.com>
2023-09-23 10:13:28 -0700

    added disable button validation

--- scripts/index.js
+++ scripts/index.js
@@ -115,11 +115,23 @@ function checkValidity(form, input) {
     input.classList.remove("modal__input_error-active");
   }
 }
+function switchButtonState(inputList, button) {
+  if (inputList.some((input) => !input.validity.valid)) {
+    button.classList.add("modal__button_disabled");
+    button.disabled = true;
+  } else {
+    button.classList.remove("modal__button_disabled");
+    button.disabled = false;
+  }
+}
 function setInputListeners(form) {
   const inputList = Array.from(form.querySelectorAll(".modal__input"));
+  const button = form.querySelector(".modal__button");
+  switchButtonState(inputList, button);
   inputList.forEach((input) => {
     input.addEventListener("input", () => {
       checkValidity(form, input);
+      switchButtonState(inputList, button);
     });
   });
 }
